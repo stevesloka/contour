9e8d4874037e37abe36ce0b0308260e6ae3fd115
Steve Sloka <steves@heptio.com>
2018-06-26 10:57:53 -0400

    Added Status field to IngressRoute
    Signed-off-by: Steve Sloka <steves@heptio.com>

--- apis/contour/v1beta1/ingressroute.go
+++ apis/contour/v1beta1/ingressroute.go
@@ -77,6 +77,14 @@ type Delegate struct {
 	Namespace string `json:"namespace"`
 }
 
+// Status reports the current state of the IngressRoute
+type Status struct {
+	CurrentStatus   string      `json:"currentStatus"`
+	Description     string      `json:"description"`
+	LastProcessTime metav1.Time `json:"lastProcessTime"`
+	Paths           []string    `json:"paths"`
+}
+
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -85,7 +93,8 @@ type IngressRoute struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
 
-	Spec IngressRouteSpec `json:"spec"`
+	Spec   IngressRouteSpec `json:"spec"`
+	Status `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
