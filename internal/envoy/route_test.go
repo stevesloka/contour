b4eef89223f2f8e9d99648bccaa3410b3e8c787a
Steve Sloka <slokas@vmware.com>
2019-04-01 21:34:21 -0400

    Remove extra aliases
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- internal/envoy/route_test.go
+++ internal/envoy/route_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/gogo/protobuf/types"
 	"github.com/google/go-cmp/cmp"
 	"github.com/heptio/contour/internal/dag"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
