309b9cb7e12b45e09c3e536062e2300a562fbc6c
Steve Sloka <slokas@vmware.com>
2021-07-07 13:23:11 -0400

    put back check for linux to fix mac builds
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- hack/actions/install-kubernetes-toolchain.sh
+++ hack/actions/install-kubernetes-toolchain.sh
@@ -54,7 +54,7 @@ esac
 echo "Installing Kubernetes toolchain..."
 
 # Install ginkgo CLI
-if [[ ${GITHUB_ACTIONS} == "true" ]]; then
+if [[ ${GITHUB_ACTIONS} == "true" && ${OS} == "linux" ]]; then
   go get github.com/onsi/ginkgo/...
   mv /home/runner/go/bin/ginkgo ${DESTDIR}/ginkgo
 fi
